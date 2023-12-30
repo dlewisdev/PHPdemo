0eebc8a55ee9c96fda9f464db258483832730ac0
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2023-12-30 15:02:11 -0500

    Created index file and added PHP tags to display text.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang=""en>
+    <head>
+        <meta charset="UTF-8">
+        <title>Demo</title>
+    </head>
+
+    <body>
+        <h1>
+        <?php 
+            echo "Hi, I'm Danielle";
+        ?>
+        </h1>
+
+        <p>
+            <?php 
+                echo "This is my first PHP webpage! Learning new technologies is always a fun challenge."
+            ?>
+        </p>
+    </body>
+    
+</html>
